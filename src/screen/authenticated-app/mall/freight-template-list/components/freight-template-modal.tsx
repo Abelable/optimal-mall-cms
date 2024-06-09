2782bc6c42a51cc36947a220762c7aed75e30567
Administrator <admin@example.com>
2024-06-09 23:55:44 +0800

    feat: 运费模板

--- src/screen/authenticated-app/mall/freight-template-list/components/freight-template-modal.tsx
+++ src/screen/authenticated-app/mall/freight-template-list/components/freight-template-modal.tsx
@@ -23,6 +23,10 @@ import {
   useFreightTemplateListQueryKey,
 } from "../util";
 import { useEffect } from "react";
+import { getCityOptions } from "utils/region-options";
+
+const regionOptions = getCityOptions();
+console.log("regionOptions", regionOptions);
 
 export const FreightTemplateModal = () => {
   const [form] = useForm();
@@ -154,40 +158,12 @@ export const FreightTemplateModal = () => {
                             >
                               <TreeSelect
                                 style={{ width: "45rem" }}
+                                maxTagCount={4}
                                 treeData={[
                                   {
-                                    title: "Node1",
-                                    value: "0-0",
-                                    key: "0-0",
-                                    children: [
-                                      {
-                                        title: "Child Node1",
-                                        value: "0-0-0",
-                                        key: "0-0-0",
-                                      },
-                                    ],
-                                  },
-                                  {
-                                    title: "Node2",
-                                    value: "0-1",
-                                    key: "0-1",
-                                    children: [
-                                      {
-                                        title: "Child Node3",
-                                        value: "0-1-0",
-                                        key: "0-1-0",
-                                      },
-                                      {
-                                        title: "Child Node4",
-                                        value: "0-1-1",
-                                        key: "0-1-1",
-                                      },
-                                      {
-                                        title: "Child Node5",
-                                        value: "0-1-2",
-                                        key: "0-1-2",
-                                      },
-                                    ],
+                                    label: "全国",
+                                    value: "0",
+                                    children: regionOptions,
                                   },
                                 ]}
                                 treeCheckable
