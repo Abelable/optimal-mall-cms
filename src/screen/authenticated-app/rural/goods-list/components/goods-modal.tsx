5e7da2c7e3f08d8614ec1d9e2c9a7aa1d4c2b5b8
Administrator <admin@example.com>
2024-08-10 22:37:52 +0800

    feat: 诚信乡村商品

--- src/screen/authenticated-app/rural/goods-list/components/goods-modal.tsx
+++ src/screen/authenticated-app/rural/goods-list/components/goods-modal.tsx
@@ -1,13 +1,16 @@
 import { Form, Image, Modal, Select } from "antd";
 import { ErrorBox } from "components/lib";
 
+import { useEffect, useState } from "react";
 import { useForm } from "antd/lib/form/Form";
+import _ from "lodash";
 import { useGoodsOptions } from "service/goods";
 import { useAddRuralGoods } from "service/ruralGoods";
 import { useRuralGoodsModal, useRuralGoodsListQueryKey } from "../util";
 
 import type { RuralRegionOption } from "types/ruralRegion";
 import styled from "@emotion/styled";
+import { GoodsOption } from "types/goods";
 
 export const RuralGoodsModal = ({
   regionOptions,
@@ -17,9 +20,17 @@ export const RuralGoodsModal = ({
   const [form] = useForm();
   const { ruralGoodsModalOpen, close } = useRuralGoodsModal();
 
+  const [keywords, setKeywords] = useState("");
+  const [options, setOptions] = useState<GoodsOption[]>([]);
   const { data: goodsOptions = [], error: goodsOptionsError } = useGoodsOptions(
-    { keywords: "" }
+    { keywords }
   );
+
+  useEffect(() => {
+    if (goodsOptions.length) {
+      setOptions(goodsOptions);
+    }
+  }, [goodsOptions]);
 
   const {
     mutateAsync,
@@ -68,8 +79,16 @@ export const RuralGoodsModal = ({
           label="商品"
           rules={[{ required: true, message: "请选择商品" }]}
         >
-          <Select mode="multiple" showSearch placeholder="请选择商品">
-            {goodsOptions.map(({ id, cover, name }) => (
+          <Select
+            onSearch={_.debounce((value) => {
+              setKeywords(value);
+              setOptions(goodsOptions);
+            }, 500)}
+            mode="multiple"
+            showSearch
+            placeholder="请选择商品"
+          >
+            {options.map(({ id, cover, name }) => (
               <Select.Option key={id} value={id}>
                 <GoodsCover src={cover} />
                 <span>{name}</span>
