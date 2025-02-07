1b34ce329b7642671a1dc81e78dfddadf2bd62e5
funguy <1209683835@qq.com>
2025-02-07 14:22:46 +0800

    feat: 订单发货

--- src/screen/authenticated-app/order-management/order-list/components/delivery-modal.tsx
+++ src/screen/authenticated-app/order-management/order-list/components/delivery-modal.tsx
@@ -1,10 +1,40 @@
-import { Form, Input, Modal, Select } from "antd";
+import {
+  Button,
+  Divider,
+  Form,
+  Input,
+  InputNumber,
+  Modal,
+  Select,
+  Table,
+} from "antd";
+import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
 
+import { useState } from "react";
 import { useForm } from "antd/lib/form/Form";
+import styled from "@emotion/styled";
+import _ from "lodash";
 import { useDeliveryOrder } from "service/order";
 import { useDeliveryModal, useOrderListQueryKey } from "../util";
 
 import type { ExpressOption } from "types/express";
+
+interface FormItem {
+  id: number;
+  type: number | undefined;
+  required: boolean;
+  name: string;
+  options: string[] | undefined;
+}
+
+const typeOptions = [
+  { id: 1, name: "单行文本框" },
+  { id: 2, name: "多行文本框" },
+  { id: 3, name: "数字输入框" },
+  { id: 4, name: "单选按钮框" },
+  { id: 5, name: "多选按钮框" },
+  { id: 6, name: "图片上传框" },
+];
 
 export const DeliveryModal = ({
   expressOptions,
@@ -17,6 +47,36 @@ export const DeliveryModal = ({
   const { mutateAsync, isLoading: mutateLoading } = useDeliveryOrder(
     useOrderListQueryKey()
   );
+
+  const [formList, setFormList] = useState<FormItem[]>([]);
+
+  const addItem = () => {
+    const defaultFormItem: Omit<FormItem, "id"> = {
+      type: undefined,
+      required: false,
+      name: "",
+      options: undefined,
+    };
+    const list = [...formList];
+    const id = list.length
+      ? list.sort((a, b) => a.id - b.id)[formList.length - 1].id + 1
+      : 1;
+    setFormList([...formList, { id, ...defaultFormItem }]);
+  };
+
+  const selectType = (id: number) => (type: number) => {
+    const list = formList.map((item: any) =>
+      item.id === id ? { ...item, type } : item
+    );
+    setFormList([...list]);
+  };
+  const setName = (id: number) =>
+    _.debounce((e: any) => {
+      const list = formList.map((item: any) =>
+        item.id === id ? { ...item, name: e.target.value } : item
+      );
+      setFormList([...list]);
+    }, 1000);
 
   const confirm = () => {
     form.validateFields().then(async () => {
@@ -42,7 +102,7 @@ export const DeliveryModal = ({
   return (
     <Modal
       forceRender={true}
-      title="发货"
+      title="订单发货"
       open={deliveryModalOpen}
       confirmLoading={mutateLoading}
       onOk={confirm}
@@ -70,6 +130,79 @@ export const DeliveryModal = ({
           <Input placeholder={"请输入快递单号"} />
         </Form.Item>
       </Form>
+      <Divider orientation="left" style={{ fontSize: "14px" }}>
+        包裹商品
+      </Divider>
+      <>
+        <Table
+          style={{ width: "100%" }}
+          rowKey="id"
+          columns={[
+            {
+              title: "商品",
+              dataIndex: "type",
+              render: (value, item) => (
+                <Select
+                  style={{ width: "240px" }}
+                  defaultValue={value}
+                  onChange={selectType(item.id)}
+                  placeholder="请选择商品"
+                >
+                  {typeOptions.map((option) => (
+                    <Select.Option key={option.id} value={option.id}>
+                      {option.name}
+                    </Select.Option>
+                  ))}
+                </Select>
+              ),
+              width: "240px",
+            },
+            {
+              title: "商品数量",
+              dataIndex: "name",
+              render: (value, item) => (
+                <InputNumber
+                  style={{ width: "120px" }}
+                  defaultValue={value}
+                  onChange={setName(item.id)}
+                  placeholder="请输入数量"
+                />
+              ),
+            },
+            {
+              render: (value, item) => (
+                <Delete
+                  onClick={() =>
+                    setFormList([
+                      ...formList.filter(
+                        (_item: FormItem) => _item.id !== item.id
+                      ),
+                    ])
+                  }
+                />
+              ),
+            },
+          ]}
+          pagination={false}
+          dataSource={formList}
+          bordered
+        />
+        <Button
+          style={{ marginTop: "2rem", width: "100%" }}
+          icon={<PlusOutlined />}
+          onClick={addItem}
+        >
+          添加商品选项
+        </Button>
+      </>
     </Modal>
   );
 };
+
+const Delete = styled(DeleteOutlined)`
+  cursor: pointer;
+  transition: color 0.3s;
+  &:hover {
+    color: red;
+  }
+`;
