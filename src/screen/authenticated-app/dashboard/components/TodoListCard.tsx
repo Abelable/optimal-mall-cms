214277d9397d5218b738418e5873c4ab441e78d1
funguy <1209683835@qq.com>
2025-02-28 22:46:10 +0800

    feat: 数据概览

--- src/screen/authenticated-app/dashboard/components/TodoListCard.tsx
+++ src/screen/authenticated-app/dashboard/components/TodoListCard.tsx
@@ -1,5 +1,6 @@
-import { Card } from "antd";
-import { PageTitle, Row } from "components/lib";
+import styled from "@emotion/styled";
+import { Badge, Card } from "antd";
+import { ButtonNoPadding, PageTitle, Row } from "components/lib";
 
 export const TodoListCard = ({ loading }: { loading: boolean }) => (
   <Card
@@ -7,8 +8,81 @@ export const TodoListCard = ({ loading }: { loading: boolean }) => (
     title={
       <Row>
         <PageTitle>代办事项</PageTitle>
+        <Badge style={{ marginLeft: "0.8rem" }} count={25} />
       </Row>
     }
     style={{ marginLeft: "2.4rem", flex: 1 }}
-  ></Card>
+  >
+    <div style={{ height: "40rem", overflowY: "scroll" }}>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+      <TodoItem>
+        <Badge status="processing" text="您有一份订单超时未出货，请及时处理" />
+        <ButtonNoPadding type="link">立即处理</ButtonNoPadding>
+      </TodoItem>
+    </div>
+  </Card>
 );
+
+const TodoItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1.2rem;
+  padding-left: 0.4rem;
+  width: 100%;
+`;
