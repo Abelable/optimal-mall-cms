bae9fec17f5c906c1c07e6947066948876ff6c2c
funguy <1209683835@qq.com>
2025-03-07 21:07:52 +0800

    feat: 商家退货、提货地址

--- src/screen/authenticated-app/mall/pickup-address-list/components/pickup-address-modal.tsx
+++ src/screen/authenticated-app/mall/pickup-address-list/components/pickup-address-modal.tsx
@@ -1,10 +1,33 @@
-import { Col, Form, Input, Modal, Row, Space } from "antd";
+import { useEffect } from "react";
+import styled from "@emotion/styled";
 import { useForm } from "antd/lib/form/Form";
-import { ErrorBox, ModalLoading } from "components/lib";
 import { useAddPickupAddress, useEditPickupAddress } from "service/merchant";
 import { usePickupAddressModal, usePickupAddressListQueryKey } from "../util";
-import { useEffect } from "react";
+
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Modal,
+  Row,
+  Select,
+  Space,
+  TimePicker,
+} from "antd";
+import { ErrorBox, ModalLoading, Row as CustomRow } from "components/lib";
 import { Map } from "components/map";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+
+const weekDayOptions = [
+  { text: "周一", value: 1 },
+  { text: "周二", value: 2 },
+  { text: "周三", value: 3 },
+  { text: "周四", value: 4 },
+  { text: "周五", value: 5 },
+  { text: "周六", value: 6 },
+  { text: "周日", value: 7 },
+];
 
 export const PickupAddressModal = ({ merchantId }: { merchantId: number }) => {
   const [form] = useForm();
@@ -68,12 +91,139 @@ export const PickupAddressModal = ({ merchantId }: { merchantId: number }) => {
         <ModalLoading />
       ) : (
         <Form form={form} layout="vertical">
-          <Form.Item
-            label="提货点名称"
-            name="name"
-            rules={[{ required: true, message: "请输入提货点名称" }]}
-          >
+          <Form.Item label="提货点名称" name="name">
             <Input placeholder={"请输入提货点名称"} />
+          </Form.Item>
+          <Form.Item label="提货时间范围">
+            <Form.List name="timeFrame">
+              {(fields, { add, remove }) => (
+                <>
+                  {fields.map(({ key, name, ...restField }) => (
+                    <CustomRow key={key} style={{ marginBottom: "2rem" }}>
+                      <Card style={{ marginRight: "1rem" }}>
+                        <CustomRow between>
+                          <Form.Item
+                            {...restField}
+                            name={[name, "startWeekDay"]}
+                            rules={[
+                              { required: true, message: "请选择开始时间" },
+                            ]}
+                          >
+                            <Select
+                              style={{ width: "20.9rem" }}
+                              placeholder="开始时间"
+                            >
+                              {weekDayOptions.map(({ text, value }) => (
+                                <Select.Option key={value} value={value}>
+                                  {text}
+                                </Select.Option>
+                              ))}
+                            </Select>
+                          </Form.Item>
+                          <Form.Item
+                            {...restField}
+                            name={[name, "endWeekDay"]}
+                            rules={[
+                              { required: true, message: "请选择结束时间" },
+                            ]}
+                          >
+                            <Select
+                              style={{ width: "20.9rem" }}
+                              placeholder="结束时间"
+                            >
+                              {weekDayOptions.map(({ text, value }) => (
+                                <Select.Option key={value} value={value}>
+                                  {text}
+                                </Select.Option>
+                              ))}
+                            </Select>
+                          </Form.Item>
+                        </CustomRow>
+                        <Form.List name={[name, "timeFrameList"]}>
+                          {(
+                            fieldsInside,
+                            { add: addInside, remove: removeInside }
+                          ) => (
+                            <TimeFrameWrap>
+                              {fieldsInside.map(
+                                ({
+                                  key: insideKey,
+                                  name: insideName,
+                                  ...insideRestField
+                                }) => (
+                                  <Space
+                                    key={insideKey}
+                                    style={{ display: "flex" }}
+                                    align="baseline"
+                                  >
+                                    <Form.Item
+                                      {...insideRestField}
+                                      name={[insideName, "openTime"]}
+                                      rules={[
+                                        {
+                                          required: true,
+                                          message: "请选择营业时间",
+                                        },
+                                      ]}
+                                    >
+                                      <TimePicker
+                                        style={{ width: "19.4rem" }}
+                                        format="HH:mm"
+                                        placeholder="营业时间"
+                                      />
+                                    </Form.Item>
+                                    <Form.Item
+                                      {...insideRestField}
+                                      name={[insideName, "closeTime"]}
+                                      rules={[
+                                        {
+                                          required: true,
+                                          message: "请选择休息时间",
+                                        },
+                                      ]}
+                                    >
+                                      <TimePicker
+                                        style={{ width: "19.4rem" }}
+                                        format="HH:mm"
+                                        placeholder="休息时间"
+                                      />
+                                    </Form.Item>
+                                    <MinusCircleOutlined
+                                      style={{ color: "#ff4d4f" }}
+                                      onClick={() => removeInside(insideName)}
+                                    />
+                                  </Space>
+                                )
+                              )}
+                              <Button
+                                type="dashed"
+                                onClick={() => addInside()}
+                                block
+                                icon={<PlusOutlined />}
+                              >
+                                添加营业时间段
+                              </Button>
+                            </TimeFrameWrap>
+                          )}
+                        </Form.List>
+                      </Card>
+                      <MinusCircleOutlined
+                        style={{ color: "#ff4d4f" }}
+                        onClick={() => remove(name)}
+                      />
+                    </CustomRow>
+                  ))}
+                  <Button
+                    type="dashed"
+                    onClick={() => add()}
+                    block
+                    icon={<PlusOutlined />}
+                  >
+                    添加提货时间
+                  </Button>
+                </>
+              )}
+            </Form.List>
           </Form.Item>
           <Form.Item
             label="提货点地址详情"
@@ -112,3 +262,15 @@ export const PickupAddressModal = ({ merchantId }: { merchantId: number }) => {
     </Modal>
   );
 };
+
+const Card = styled.div`
+  padding: 1rem;
+  flex: 1;
+  border: 1px solid #ddd;
+  border-radius: 1rem;
+`;
+const TimeFrameWrap = styled.div`
+  margin-top: 1rem;
+  padding-top: 1rem;
+  border-top: 1px solid #ddd;
+`;
