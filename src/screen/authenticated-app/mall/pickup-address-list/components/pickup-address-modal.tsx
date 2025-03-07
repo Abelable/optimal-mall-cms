807e852b99832f779c4168a9ef6156fb59143b01
funguy <1209683835@qq.com>
2025-03-07 17:57:41 +0800

    feat: 商家退货、提货地址

---
+++ src/screen/authenticated-app/mall/pickup-address-list/components/pickup-address-modal.tsx
@@ -0,0 +1,68 @@
+import { Form, Input, Modal } from "antd";
+import { useForm } from "antd/lib/form/Form";
+import { ErrorBox, ModalLoading } from "components/lib";
+import { useAddPickupAddress, useEditPickupAddress } from "service/merchant";
+import { usePickupAddressModal, usePickupAddressListQueryKey } from "../util";
+import { useEffect } from "react";
+
+export const PickupAddressModal = () => {
+  const [form] = useForm();
+  const {
+    pickupAddressModalOpen,
+    editingPickupAddress,
+    editingPickupAddressId,
+    isLoading,
+    close,
+  } = usePickupAddressModal();
+
+  const useMutateRole = editingPickupAddressId
+    ? useEditPickupAddress
+    : useAddPickupAddress;
+  const {
+    mutateAsync,
+    isLoading: mutateLoading,
+    error,
+  } = useMutateRole(usePickupAddressListQueryKey());
+
+  useEffect(() => {
+    form.setFieldsValue(editingPickupAddress);
+  }, [editingPickupAddress, form]);
+
+  const confirm = () => {
+    form.validateFields().then(async () => {
+      await mutateAsync({ ...editingPickupAddress, ...form.getFieldsValue() });
+      closeModal();
+    });
+  };
+
+  const closeModal = () => {
+    form.resetFields();
+    close();
+  };
+
+  return (
+    <Modal
+      forceRender={true}
+      title={editingPickupAddressId ? "编辑提货地址" : "新增提货地址"}
+      open={pickupAddressModalOpen}
+      confirmLoading={mutateLoading}
+      onOk={confirm}
+      onCancel={closeModal}
+    >
+      <ErrorBox error={error} />
+      {isLoading ? (
+        <ModalLoading />
+      ) : (
+        <Form form={form} layout="vertical">
+          <Form.Item
+            label="提货地址名称"
+            name="name"
+            rules={[{ required: true, message: "请输入提货地址名称" }]}
+          >
+            <Input placeholder={"请输入提货地址名称"} />
+          </Form.Item>
+        </Form>
+      )}
+    </Modal>
+  );
+};
---
+++ src/screen/authenticated-app/mall/pickup-address-list/util.ts
@@ -0,0 +1,63 @@
+import { useCallback, useMemo } from "react";
+import { useSetUrlSearchParams, useUrlQueryParams } from "utils/url";
+import { usePickupAddress } from "service/merchant";
+
+export const usePickupAddressListSearchParams = () => {
+  const [params, setParams] = useUrlQueryParams([
+    "merchangId",
+    "page",
+    "limit",
+  ]);
+  return [
+    useMemo(
+      () => ({
+        page: Number(params.page) || 1,
+        limit: Number(params.limit) || 10,
+        ...params,
+      }),
+      [params]
+    ),
+    setParams,
+  ] as const;
+};
+
+export const usePickupAddressListQueryKey = () => {
+  const [params] = usePickupAddressListSearchParams();
+  return ["merchant_refund_address_list", params];
+};
+
+export const usePickupAddressModal = () => {
+  const [{ pickupAddressCreate }, setPickupAddressModalOpen] =
+    useUrlQueryParams(["pickupAddressCreate"]);
+  const [{ editingPickupAddressId }, setEditingPickupAddressId] =
+    useUrlQueryParams(["editingPickupAddressId"]);
+  const setUrlParams = useSetUrlSearchParams();
+  const { data: editingPickupAddress, isLoading } = usePickupAddress(
+    Number(editingPickupAddressId)
+  );
+
+  const open = useCallback(
+    () => setPickupAddressModalOpen({ pickupAddressCreate: true }),
+    [setPickupAddressModalOpen]
+  );
+  const startEdit = useCallback(
+    (id: number) =>
+      setEditingPickupAddressId({ editingPickupAddressId: `${id}` }),
+    [setEditingPickupAddressId]
+  );
+  const close = useCallback(
+    () => setUrlParams({ pickupAddressCreate: "", editingPickupAddressId: "" }),
+    [setUrlParams]
+  );
+
+  return {
+    pickupAddressModalOpen:
+      pickupAddressCreate === "true" || !!editingPickupAddressId,
+    editingPickupAddressId,
+    editingPickupAddress,
+    isLoading,
+    open,
+    startEdit,
+    close,
+  };
+};
